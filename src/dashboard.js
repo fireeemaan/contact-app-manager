5a5156e1a5c515065a84bae6b59601591da59485
fireeemaan <firmancakep222@gmail.com>
2024-03-29 22:50:57 +0700

    uhuy

--- src/dashboard.js
+++ src/dashboard.js
@@ -19,12 +19,34 @@ thElements.forEach((th) => {
     th.classList.add('py-3');
 });
 
+Promise.all([
+    fetch('../assets/img/delete-icon.svg').then(response => response.text()),
+    fetch('../assets/img/edit-icon.svg').then(response => response.text())
+])
+    .then(([deleteIcon, editIcon]) => {
 
-tdButtons.forEach((td) => {
-    const buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'Delete';
+        const lastTdElements = document.querySelectorAll('tbody tr td:last-child');
 
-    td.appendChild(buttonDelete);
-});
+        lastTdElements.forEach((td) => {
+
+            const buttonDelete = document.createElement('button');
+            buttonDelete.innerHTML = deleteIcon;
+            buttonDelete.className = 'p-2 bg-red-500 rounded-lg mr-2 hover:bg-red-600';
+
+            const buttonEdit = document.createElement('button');
+            buttonEdit.innerHTML = editIcon;
+            buttonEdit.className = 'p-2 bg-blue-500 rounded-lg hover:bg-blue-600';
+
+            td.appendChild(buttonDelete);
+            td.appendChild(buttonEdit);
+
+        });
+        const svgActions = document.querySelectorAll('tbody tr td svg')
+        svgActions.forEach((svg) => {
+            svg.setAttribute('width', '20');
+            svg.setAttribute('height', '20');
+        });
+    });
+
 
 
