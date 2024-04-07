6bd370ceeb0ae0b3506cbb6baf3453ee226d15ab
fireeemaan <firmancakep222@gmail.com>
2024-04-07 21:10:19 +0700

    added dashboard.php

---
+++ src/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+class DashboardContact
+{
+    static function select()
+    {
+
+    }
+    static function insert()
+    {
+
+    }
+    static function update()
+    {
+
+    }
+    static function delete()
+    {
+
+    }
+}
+
